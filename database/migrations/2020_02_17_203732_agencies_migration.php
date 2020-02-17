6a37da5328efdd21eacc1a8a7b14d437a395e194
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-17 22:19:16 +0000

    agency migration
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ database/migrations/2020_02_17_203732_agencies_migration.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AgenciesMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = 'agencies';
+        Schema::dropIfExists($tableName);
+        Schema::create($tableName, function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('name', 70);
+            $table->double('stars', 4, 2)->default(0);
+            $table->integer('review')->unsigned()->default(0);
+            $table->string('address', 200);
+            $table->string('city', 70);
+            $table->string('postcode', 10);
+            $table->string('phone', 20);
+            $table->string('website', 70);
+            $table->text('description')->default(" ");
+            $table->timestamps();
+            $table->softDeletes();
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
