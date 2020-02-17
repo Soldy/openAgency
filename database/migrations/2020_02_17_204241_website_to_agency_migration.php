66a0b17bab4436cd5fe5d5449fc177a4959b9386
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-17 22:29:01 +0000

    website to agencyy table migration
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ database/migrations/2020_02_17_204241_website_to_agency_migration.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class WebsiteToAgencyMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = 'websiteToAgency';
+        Schema::dropIfExists($tableName);
+        Schema::create($tableName, function (Blueprint $table) {
+            $table->unsignedBigInteger('websiteId');
+            $table->unsignedBigInteger('agencyId');
+            $table->foreign('websiteId')->references('id')->on('websites');
+            $table->foreign('agencyId')->references('id')->on('agencies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
