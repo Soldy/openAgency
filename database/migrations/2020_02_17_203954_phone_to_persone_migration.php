708e1267c307e1ea1605beed70b98ce9fae9ee7e
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-17 22:23:09 +0000

    phones to persone table migration
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ database/migrations/2020_02_17_203954_phone_to_persone_migration.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PhoneToPersoneMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = 'phoneToPerson';
+        Schema::dropIfExists($tableName);
+        Schema::create($tableName, function (Blueprint $table) {
+            $table->unsignedBigInteger('phoneId');
+            $table->unsignedBigInteger('personId');
+            $table->foreign('phoneId')->references('id')->on('phones');
+            $table->foreign('personId')->references('id')->on('persones');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
