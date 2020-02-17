156802f5c58707b7224c7bb757ca859aaeac3ba8
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-17 22:31:58 +0000

    applications to agency table migration
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ database/migrations/2020_02_17_204550_application_to_agency_migration.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ApplicationToAgencyMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = 'applicationToAgency';
+        Schema::dropIfExists($tableName);
+        Schema::create($tableName, function (Blueprint $table) {
+            $table->unsignedBigInteger('applicationId');
+            $table->unsignedBigInteger('agencyId');
+            $table->foreign('applicationId')->references('id')->on('applications');
+            $table->foreign('agencyId')->references('id')->on('agencies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
