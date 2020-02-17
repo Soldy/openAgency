33b1cab75d86ab0bfbd31b99c8c19922a0cdd1da
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-17 22:31:00 +0000

    event to applications table migration
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ database/migrations/2020_02_17_204521_event_to_application_migration.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EventToApplicationMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = 'eventToApplication';
+        Schema::dropIfExists($tableName);
+        Schema::create($tableName, function (Blueprint $table) {
+            $table->unsignedBigInteger('eventId');
+            $table->unsignedBigInteger('applicationId');
+            $table->foreign('eventId')->references('id')->on('events');
+            $table->foreign('applicationId')->references('id')->on('applications');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
