6cce28bc881fcf9fb7f3542bb0d19c314932e550
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-18 12:14:17 +0000

    add category model
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ app/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Category extends Model
+{
+    use SoftDeletes;
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+    protected $table = 'agencies';
+    protected $primaryKey = 'id';
+
+
+}
