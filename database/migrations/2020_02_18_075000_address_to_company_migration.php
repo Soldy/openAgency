b5e57d782c2a736fea22d2df3d80105be800f5a9
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-18 12:08:40 +0000

    addresses to company migration
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ database/migrations/2020_02_18_075000_address_to_company_migration.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddressToCompanyMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = 'addressToCompany';
+        Schema::dropIfExists($tableName);
+        Schema::create($tableName, function (Blueprint $table) {
+            $table->unsignedBigInteger('addressId');
+            $table->unsignedBigInteger('companyId');
+            $table->string('comment');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('addressId')->references('id')->on('addresses');
+            $table->foreign('companyId')->references('id')->on('companies');
+              $table->bigIncrements('id')->unsigned();
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
