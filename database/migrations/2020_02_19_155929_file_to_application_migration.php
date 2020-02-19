e5d368281697e6efa2688013ec15ffac746c25a4
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-19 19:41:26 +0000

    file to application migration
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ database/migrations/2020_02_19_155929_file_to_application_migration.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FileToApplicationMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = 'fileToApplication';
+        Schema::dropIfExists($tableName);
+        Schema::create($tableName, function (Blueprint $table) {
+            $table->unsignedBigInteger('fileId');
+            $table->unsignedBigInteger('applicationId');
+            $table->string('comment');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('fileId')->references('id')->on('files');
+            $table->foreign('applicationId')->references('id')->on('applications');
+              $table->bigIncrements('id')->unsigned();
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
