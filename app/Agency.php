165f22cee2f4176cc6b6dcab37a08ae459190ab3
Soldy <4786022+Soldy@users.noreply.github.com>
2020-03-02 21:43:06 +0000

    Agency many to many relations
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- app/Agency.php
+++ app/Agency.php
@@ -28,6 +28,33 @@ class Agency extends Model
               ])
               ->toArray();
     }
+    public function applications(){
+         $this->belongsToMany('App\Application', 'applicationToAgency', 'applicationId', 'agencyId');
+    }
+    public function addresses(){
+         $this->belongsToMany('App\Address', 'addressToAgency', 'addressId', 'agencyId');
+    }
+    public function cases(){
+         $this->belongsToMany('App\Case', 'caseToAgency', 'caseId', 'agencyId');
+    }
+    public function categorys(){
+         $this->belongsToMany('App\Category', 'categoryToAgency', 'categoryId', 'agencyId');
+    }
+    public function events(){
+         $this->belongsToMany('App\Event', 'eventToAgency', 'eventId', 'agencyId');
+    }
+    public function files(){
+         $this->belongsToMany('App\Files', 'fileToAgency', 'fileId', 'agencyId');
+    }
+    public function phones(){
+         $this->belongsToMany('App\Phone', 'phoneToAgency', 'phoneId', 'agencyId');
+    }
+    public function tickets(){
+         $this->belongsToMany('App\Ticket', 'ticketToAgency', 'ticketId', 'agencyId');
+    }
+    public function websites(){
+         $this->belongsToMany('App\Website', 'websiteToAgency', 'websiteId', 'agencyId');
+    }
     function __construct(){
     }
 }
---
+++ app/Asset.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Asset extends Model
+{
+    use SoftDeletes;
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+    protected $table = 'agencies';
+    protected $primaryKey = 'id';
+    public function addresses(){
+         $this->belongsToMany('App\Address', 'addressToAsset', 'addressId', 'assetId');
+    }
+    public function cases(){
+         $this->belongsToMany('App\Case', 'assetToCase', 'caseId', 'assetId');
+    }
+    public function categorys(){
+         $this->belongsToMany('App\Category', 'categoryToAsset', 'categoryId', 'assetId');
+    }
+    public function files(){
+         $this->belongsToMany('App\File', 'fileToAsset', 'fileId', 'assetId');
+    }
+    public function websites(){
+         $this->belongsToMany('App\Website', 'websiteToAsset', 'websiteId', 'assetId');
+    }
+}
