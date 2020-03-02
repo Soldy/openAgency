d60d1583546c96a3234b8d2bc186844eb856055f
Soldy <4786022+Soldy@users.noreply.github.com>
2020-03-02 21:43:21 +0000

    Categoryy many to many relations
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- app/Category.php
+++ app/Category.php
@@ -14,6 +14,42 @@ class Category extends Model
     const UPDATED_AT = 'updated_at';
     protected $table = 'agencies';
     protected $primaryKey = 'id';
-
-
+    public function addresses (){
+         $this->belongsToMany('App\Address', 'categoryToAddress', 'addressId', 'categoryId');
+    }
+    public function agencys (){
+         $this->belongsToMany('App\Agency', 'categoryToAgency', 'agencyId', 'categoryId');
+    }
+    public function  assets(){
+         $this->belongsToMany('App\Asset', 'categoryToAsset', 'assetId', 'categoryId');
+    }
+    public function applications(){
+         $this->belongsToMany('App\Application', 'categoryToApplication', 'applicationId', 'categoryId');
+    }
+    public function cases (){
+         $this->belongsToMany('App\Case', 'categoryToCase', 'caseId', 'categoryId');
+    }
+    public function companys (){
+         $this->belongsToMany('App\Company', 'categoryToCompany', 'companyId', 'categoryId');
+    }
+    public function emails (){
+         $this->belongsToMany('App\Email', 'categoryToEmail', 'emailId', 'categoryId');
+    }
+    public function events (){
+         $this->belongsToMany('App\Event', 'categoryToEvent', 'eventId', 'categoryId');
+    }
+    public function persons (){
+         $this->belongsToMany('App\Person', 'categoryToPerson', 'personId', 'categoryId');
+    }
+    public function phones (){
+         $this->belongsToMany('App\Phone', 'categoryToPhone', 'phoneId', 'categoryId');
+    }
+    public function tickets (){
+         $this->belongsToMany('App\Ticket', 'categoryToTicket', 'ticketId', 'categoryId');
+    }
+    public function websites (){
+         $this->belongsToMany('App\Website', 'categoryToWebsite', 'websiteId', 'categoryId');
+    }
 }
+
+
