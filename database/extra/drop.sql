ea11d620b6fcc6dff9878d2a80fd5abe0587350f
Soldy <4786022+Soldy@users.noreply.github.com>
2020-03-05 21:28:42 +0000

    drop all table
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ database/extra/drop.sql
@@ -0,0 +1,44 @@
+
+DROP TABLE addressToAgency ; 
+DROP TABLE addressToCompany ; 
+DROP TABLE addressToEvent ; 
+DROP TABLE addressToPerson ; 
+DROP TABLE applicationToAgency ; 
+DROP TABLE applicationToCompany ; 
+DROP TABLE applicationToPerson; 
+DROP TABLE categoryToAddress ;
+DROP TABLE categoryToAgency ;
+DROP TABLE categoryToApplication ;
+DROP TABLE categoryToCompany ; 
+DROP TABLE categoryToEmail ; 
+DROP TABLE categoryToEvent ; 
+DROP TABLE categoryToPerson ; 
+DROP TABLE categoryToPhone ;
+DROP TABLE categoryToWebsite ;
+DROP TABLE emailToPerson; 
+DROP TABLE eventToAgency ; 
+DROP TABLE eventToApplication ; 
+DROP TABLE eventToCompany ; 
+DROP TABLE eventToPerson ; 
+DROP TABLE phoneToAgency ; 
+DROP TABLE phoneToCompany ; 
+DROP TABLE phoneToPerson ; 
+DROP TABLE websiteToAgency ; 
+DROP TABLE websiteToCompany; 
+DROP TABLE websiteToPerson; 
+DROP TABLE websites; 
+DROP TABLE users ;
+DROP TABLE phones ; 
+DROP TABLE persones ; 
+DROP TABLE password_resets;
+DROP TABLE migrations; 
+DROP TABLE failed_jobs ; 
+DROP TABLE events ; 
+DROP TABLE emails; 
+DROP TABLE companies; 
+DROP TABLE categories; 
+DROP TABLE applications ; 
+DROP TABLE agencies;
+DROP TABLE addresses;
+
+
