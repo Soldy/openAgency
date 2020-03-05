eac687683c53ab63a2132c7823f32de5a19201d0
Soldy <4786022+Soldy@users.noreply.github.com>
2020-03-05 22:37:26 +0000

    add company model
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- app/Company.php
+++ app/Company.php
@@ -13,5 +13,34 @@ class Company extends Model
     const UPDATED_AT = 'updated_at';
     protected $table = 'companies';
     protected $primaryKey = 'id';
-    //
+    public function addresses(){
+         $this->belongsToMany('App\Address', 'addressToCompany', 'addressId', 'companyId');
+    }
+    public function applications (){
+         $this->belongsToMany('App\Application', 'applicationToCompany', 'applicationId', 'companyId');
+    }
+    public function cases (){
+         $this->belongsToMany('App\Case', 'companyToCase', 'caseId', 'companyId');
+    }
+    public function categorys (){
+         $this->belongsToMany('App\Company', 'categoryToCompany', 'categoryId', 'companyId');
+    }
+    public function emails (){
+         $this->belongsToMany('App\Email', 'emailToCase', 'emailId', 'companyId');
+    }
+    public function events (){
+         $this->belongsToMany('App\Event', 'eventToCompany', 'eventId', 'companyId');
+    }
+    public function files (){
+         $this->belongsToMany('App\File', 'fileToCompany', 'fileId', 'companyId');
+    }
+    public function phones (){
+         $this->belongsToMany('App\Phone', 'phoneToCompany', 'phoneId', 'companyId');
+    }
+    public function tickets (){
+         $this->belongsToMany('App\Ticket', 'ticketToCompany', 'ticketId', 'companyId');
+    }
+    public function websites (){
+         $this->belongsToMany('App\Website', 'websiteToCompany', 'websiteId', 'companyId');
+    }
 }
