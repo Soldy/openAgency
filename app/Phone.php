c3176dcacf3e6dec9525dd5dbbe70d29d3af25a3
Soldy <4786022+Soldy@users.noreply.github.com>
2020-03-06 09:59:25 +0000

    phone model
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- app/Phone.php
+++ app/Phone.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
 class Phone extends Model
 {
     use SoftDeletes;
@@ -13,18 +14,16 @@ class Phone extends Model
     const UPDATED_AT = 'updated_at';
     protected $table = 'phones';
     protected $primaryKey = 'id';
-    public static function justAdd($country, $number, $extension){
-         $old = self::where('countryCode', $country)
-             ->where('number',  $number)
-             ->where('extension', $extension)
-             ->get()->toArray();
-         if(count($old)>0)
-             return $old[0]['id'];
-        $phone = new self;
-        $phone->countryCode = $country;
-        $phone->number = $number;
-        $phone->extension = $extension;
-        $phone->save();
-        return $phone->id;
+    public function agencies (){
+         $this->belongsToMany('App\Agency', 'phoneToAgency', 'agencyId', 'phoneId');
+    }
+    public function categories (){
+         $this->belongsToMany('App\Category', 'categoryToPhone', 'categoryId', 'phoneId');
+    }
+    public function companies (){
+         $this->belongsToMany('App\Company', 'phoneToCompany', 'companyId', 'phoneId');
+    }
+    public function persones (){
+         $this->belongsToMany('App\Person', 'phoneToPerson', 'personId', 'phoneId');
     }
 }
