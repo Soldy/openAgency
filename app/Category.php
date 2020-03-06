a22d68bbc3360821a55367c3c0599c331a6bc7f1
Soldy <4786022+Soldy@users.noreply.github.com>
2020-03-06 09:58:40 +0000

    category model
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- app/Category.php
+++ app/Category.php
@@ -12,7 +12,7 @@ class Category extends Model
     use SoftDeletes;
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
-    protected $table = 'agencies';
+    protected $table = 'categories';
     protected $primaryKey = 'id';
     public function addresses (){
          $this->belongsToMany('App\Address', 'categoryToAddress', 'addressId', 'categoryId');
